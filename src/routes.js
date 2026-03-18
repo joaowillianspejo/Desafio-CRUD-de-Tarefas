617649e58428e891b1b337121927279773a67351
João Willian Belarmino Spejo <joaowillian.spejo@outlook.com>
2026-03-18 12:22:47 -0300

    first commit

---
+++ src/routes.js
@@ -0,0 +1,62 @@
+import { randomUUID } from 'node:crypto';
+import { Database } from './database.js';
+import { buildRoutePath } from './utils/build-route-path.js';
+
+const database = new Database();
+
+export const routes = [
+  {
+    method: 'POST',
+    path: buildRoutePath('/tasks'),
+    handler: (request, response) => {
+      const { title, description } = request.body;
+
+      const tasks = {
+        id: randomUUID(),
+        title: title,
+        description: description,
+        completed_at: null,
+        created_at: new Date(),
+        updated_at: new Date(),
+      };
+
+      database.insert('tasks', tasks);
+
+      return response.writeHead(201).end();
+    },
+  },
+  {
+    method: 'GET',
+    path: buildRoutePath('/tasks'),
+    handler: (request, response) => {
+      const { search } = request.query;
+
+      const tasks = database.select(
+        'tasks',
+        search
+          ? {
+              title: search,
+              description: search,
+            }
+          : null,
+      );
+
+      return response.end(JSON.stringify(tasks));
+    },
+  },
+  {
+    method: 'PUT',
+    path: buildRoutePath('/tasks/:id'),
+    handler: (request, response) => {},
+  },
+  {
+    method: 'DELETE',
+    path: buildRoutePath('/tasks/:id'),
+    handler: (request, response) => {},
+  },
+  {
+    method: 'PATCH',
+    path: buildRoutePath('/tasks/:id/completeat'),
+    handler: (request, response) => {},
+  },
+];
